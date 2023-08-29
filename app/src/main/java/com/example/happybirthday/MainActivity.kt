d864ff84aff7c475243ce6ed73318c86b39cfbfb
samira <samirahadjri@gmail.com>
2023-08-29 21:25:04 +0100

    aj

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -1,15 +1,24 @@
 package com.example.happybirthday
-
+import androidx.compose.foundation.layout.Column
 import android.os.Bundle
+import android.os.Message
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalMapOf
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.sp
+
 import com.example.happybirthday.ui.theme.HappyBirthdayTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,26 +30,52 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
+
+
                 ) {
-                    Greeting("Android")
+                    GreetingText(" Happy Birthday samoura ", from = "From SamiraHadj")
                 }
             }
         }
     }
+
+
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
-@Preview(showBackground = true)
+
+
 @Composable
-fun GreetingPreview() {
-    HappyBirthdayTheme {
-        Greeting("Android")
+fun GreetingText(message: String, from: String, modifier: Modifier = Modifier) {
+    Column (modifier = modifier)
+    {
+        Text(
+                text = message,
+        fontSize = 100.sp,
+        lineHeight = 116.sp
+        )
+        Text(
+            text = from,
+            fontSize = 36.sp
+        )
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
